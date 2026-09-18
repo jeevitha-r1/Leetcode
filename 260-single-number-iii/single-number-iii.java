d995f1be3c3978f8f9c0ddfeac780c78299f77be
Jeevitha R <rmjc2006@gmail.com>
2026-09-18 12:04:50 +0530

    Time: 1 ms (100.00%) | Memory: 48 MB (40.08%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xorAll=0;
+        for(int num:nums)
+        xorAll^=num;
+        int diffBit=xorAll & (-xorAll);
+        int a =0, b=0;
+        for(int num:nums)
+        {
+            if ((num & diffBit) !=0)
+            a^=num;
+            else
+
+            b^=num;
+        }
+        return new int[]{a,b};
+
+        }
+    }
